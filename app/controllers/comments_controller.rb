8d5d86e0de32f96f68aa9930d42c5523316246d6
hirom119 <71484414+hirom119@users.noreply.github.com>
2020-11-25 13:56:53 +0900

    コメント機能実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def index
+    @comments = Comment.order("creatd_at DESC")
+  end
+
+  def new
+    @comments = Comment.all
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(text: params[:comment][:text],post_id:params[:post_id],user_id:current_user.id)
+    user = User.find(current_user.id)
+    if @comment.save
+      ActionCable.server.broadcast 'comment_channel',
+       content: @comment,
+       team: user.team
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_102700) do
+ActiveRecord::Schema.define(version: 2020_11_24_003000) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.integer "gender", null: false
     t.integer "acerage_age", null: false
     t.integer "recruitment_team", null: false
     t.integer "sports", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.text "text"
+    t.integer "post_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
