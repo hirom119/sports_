a71e5707dcaf00f9a3d89fdbe358bbc30000256b
hirom119 <71484414+hirom119@users.noreply.github.com>
2020-11-30 14:38:59 +0900

    削除機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post,only: [:edit,:show,:update]
+  before_action :set_post,only: [:edit,:show,:update,:destroy]
   def index
     @posts = Post.all
   end
@@ -31,6 +31,11 @@ class PostsController < ApplicationController
     else 
       render :edid
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
